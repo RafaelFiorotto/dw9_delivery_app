69f01934f8ee77adbb88a812f34807e65b7cef13
Rafael Fiorotto <rafaelfiorotto@hotmail.com>
2023-03-15 17:57:18 -0300

    add shopping bag 1:40:00

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_controller.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_state.dart';
 import 'package:dw9_delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
+import 'package:dw9_delivery_app/app/pages/home/widgets/shopping_bag_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -23,37 +24,47 @@ class _HomePageState extends BaseState<HomePage, HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppbar(),
-      body: BlocConsumer<HomeController, HomeState>(
-        listener: (context, state) {
-          state.status.matchAny(
-            any: () => hideLoader(),
-            loading: () => showLoader(),
-            error: () {
-              hideLoader();
-              showError(state.errorMessage ?? 'Ocorreu um erro');
-            },
-          );
-        },
-        buildWhen: (previous, current) => current.status.matchAny(
-          any: () => false,
-          initial: () => true,
-          loaded: () => true,
-        ),
-        builder: (context, state) {
-          return Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: state.products.length,
-                  itemBuilder: (context, index) {
-                    final product = state.products[index];
-                    return DeliveryProductTile(product: product);
-                  },
+      body: SafeArea(
+        child: BlocConsumer<HomeController, HomeState>(
+          listener: (context, state) {
+            state.status.matchAny(
+              any: () => hideLoader(),
+              loading: () => showLoader(),
+              error: () {
+                hideLoader();
+                showError(state.errorMessage ?? 'Ocorreu um erro');
+              },
+            );
+          },
+          buildWhen: (previous, current) => current.status.matchAny(
+            any: () => false,
+            initial: () => true,
+            loaded: () => true,
+          ),
+          builder: (context, state) {
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: state.products.length,
+                    itemBuilder: (context, index) {
+                      final product = state.products[index];
+                      final orders = state.shoppingBag
+                          .where((order) => order.product == product);
+                      return DeliveryProductTile(
+                        product: product,
+                        orderProduct: orders.isNotEmpty ? orders.first : null,
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
+                Visibility(
+                    visible: state.shoppingBag.isNotEmpty,
+                    child: const ShoppingBagWidget())
+              ],
+            );
+          },
+        ),
       ),
     );
   }
---
+++ lib/app/pages/home/widgets/shopping_bag_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShoppingBagWidget extends StatelessWidget {
+  const ShoppingBagWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Shopping Bag');
+  }
+}
