418ee158d44b95e6f772e2ff4d8e2d4b9258ac87
Rafael Fiorotto <rafaelfiorotto@hotmail.com>
2023-03-17 17:17:53 -0300

    register funtion aula 4 38:30

--- lib/app/pages/auth/register/register_page.dart
+++ lib/app/pages/auth/register/register_page.dart
@@ -1,67 +1,141 @@
+import 'package:dw9_delivery_app/app/core/ui/base_state/base_state.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
+import 'package:dw9_delivery_app/app/pages/auth/register/register_controller.dart';
+import 'package:dw9_delivery_app/app/pages/auth/register/register_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:validatorless/validatorless.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends BaseState<RegisterPage, RegisterController> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameEC = TextEditingController();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameEC.dispose();
+    _emailEC.dispose();
+    _passwordEC.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: DeliveryAppbar(),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Cadastro',
-                    style: context.textStyles.textTitle,
-                  ),
-                  Text(
-                    'Preencha os campos abaixo apara criar sua conta!',
-                    style: context.textStyles.textMedium,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Nome'),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'E-mail'),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Senha'),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(labelText: 'Confirme a senha'),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Center(
-                    child: DeliveryButton(
-                      label: 'REGISTRAR',
-                      width: double.infinity,
-                      onPressed: () {},
+    return BlocListener<RegisterController, RegisterState>(
+      listener: (context, state) {
+        state.status.matchAny(
+          any: () => hideLoader(),
+          register: () => showLoader(),
+          error: () {
+            hideLoader();
+            showLoader();
+          },
+          success: () {
+            hideLoader();
+            showSucces('Cadastro realizado com sucesso!');
+            Navigator.pop(context);
+          },
+        );
+      },
+      child: Scaffold(
+        appBar: DeliveryAppbar(),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Cadastro',
+                      style: context.textStyles.textTitle,
                     ),
-                  )
-                ],
+                    Text(
+                      'Preencha os campos abaixo apara criar sua conta!',
+                      style: context.textStyles.textMedium,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Nome'),
+                      validator: Validatorless.required('Nome Obrigatório'),
+                      controller: _nameEC,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'E-mail'),
+                      controller: _emailEC,
+                      validator: Validatorless.multiple(
+                        [
+                          Validatorless.required('E-mail Obrigatório'),
+                          Validatorless.email('E-mail Inválido'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Senha'),
+                      obscureText: true,
+                      controller: _passwordEC,
+                      validator: Validatorless.multiple(
+                        [
+                          Validatorless.required('Senha Obrigatória'),
+                          Validatorless.min(
+                              6, 'A senha deve conter pelo menos 6 caracteres'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration:
+                          const InputDecoration(labelText: 'Confirme a senha'),
+                      obscureText: true,
+                      validator: Validatorless.multiple(
+                        [
+                          Validatorless.required('Confirme sua senha!'),
+                          Validatorless.compare(
+                              _passwordEC, 'Senhas não batem!'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Center(
+                      child: DeliveryButton(
+                        label: 'REGISTRAR',
+                        width: double.infinity,
+                        onPressed: () {
+                          final valid =
+                              _formKey.currentState?.validate() ?? false;
+                          if (valid) {
+                            controller.register(
+                                _nameEC.text, _emailEC.text, _passwordEC.text);
+                          }
+                        },
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
